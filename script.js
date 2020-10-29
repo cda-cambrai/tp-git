22b03e2c1d51437057fdbc7e2e934b675465afa1
AnassTam <anass59000@gmail.com>
2020-10-29 15:57:36 +0100

    Maj1 header

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('navbar-brand')[0].addEventListener('click', function () {
+    var h1 = document.getElementsByClassName('navbar-brand')[0];
+    h1.style.color = "#a10535";
+});
